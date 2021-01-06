9a4721476870047e03e7e07c8b88a9a72be0f349
Oliver Staub <ostaubzug@gmail.com>
2021-01-06 21:04:58 +0100

    day1

---
+++ bin/dataandvariables.dart
@@ -0,0 +1,17 @@
+// all data types are objects! default values = null
+
+//! hex values can be assigned to an integer too
+
+void main() {
+  int score = 23;
+
+  var count = 23; //is defined as an integer automatically
+  double percentage = 22.2575433;
+  String name = "oli";
+  var company = "google";
+  bool isvalid = true;
+
+  print(score);
+  print(percentage);
+  print(name);
+}
---
+++ bin/helloworld.dart
@@ -0,0 +1,8 @@
+//! The main function is the starting point of the program
+void main() {
+  //here comes the code
+  print("Hello World");
+  print("this is my first dart application");
+  print(4 / 2);
+  print(false);
+}
