b2dc43275970c578ef7ccd017782f92928e6672d
Oliver Staub <ostaubzug@gmail.com>
2021-01-22 17:31:45 +0100

    done

---
+++ bin/flutter_course_dart/lection1.dart
@@ -0,0 +1,17 @@
+void main() {
+  print(addNumbers(5.3, 2.2));
+  var newVariable = addNumbers(3, 3);
+  print(newVariable);
+
+  var p1 = Person();
+  print(p1.age);
+}
+
+double addNumbers(double num1, double num2) {
+  return num1 + num2;
+}
+
+class Person {
+  String name = "Max";
+  int age = 30;
+}
